fed254275bb0c8efffe6566651b7498b93ee2f7d
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-14 09:26:26 -0500

    Imported 'styles.scss' (SCSS entrypoint) into 'index.html' file.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ var ReactDOM = require( 'react-dom' );
 // IMPORT PROJECT FILES
 // --------------------------------------------------
 require( './index.html' );
+require( './styles/styles.scss' );
 
 import App from './app/app.js';
 
