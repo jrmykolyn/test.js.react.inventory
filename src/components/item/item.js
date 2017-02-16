6796abbd8e900b2d66e1e50f95a435cc05c0df70
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-16 09:08:09 -0500

    Updated 'Item' logic: background color now based on item durability.

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -3,6 +3,11 @@
 // --------------------------------------------------
 var React = require( 'react' );
 
+// --------------------------------------------------
+// DECLARE VARS
+// --------------------------------------------------
+var colors = [ 'firebrick', 'tomato', '#fbe64c', 'palegreen', 'mediumspringgreen' ];
+
 
 // --------------------------------------------------
 // COMPONENT
@@ -14,11 +19,27 @@ class Item extends React.Component {
 
 
     render() {
+        var styles = {
+            backgroundColor: this.getBgColor( this.props.data.durability )
+        };
+
         return (
-            <article className="item" onClick={ ( e ) => { this.triggerModalWithData(); } }>
+            <article className="item" style={ styles } onClick={ ( e ) => { this.triggerModalWithData(); } }>
                 <p>{ this.props.data.name || 'Fallback' }</p>
             </article>
         );
+    }
+
+
+    getBgColor( durability ) {
+        console.log( 'INSIDE `getBgColor()`' ); /// TEMP
+        console.log( durability ); /// TEMP
+
+        if ( !durability || typeof durability !== 'number' ) {
+            return '';
+        }
+
+        return ( colors[ durability - 1 ] ) ? colors[ durability - 1 ] : '';
     }
 
 
